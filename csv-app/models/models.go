6ea85e015547c73eeb678fa2feb42158cddbf8b3
anudevSaraswat <anudev@tech-x.com>
2025-01-18 18:38:01 +0530

    in progress: User and Response structs added

---
+++ csv-app/models/models.go
@@ -0,0 +1,15 @@
+package models
+
+// this struct can be used to scan user data in and for sending filters for querying data
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	DOB   string `json:"dob"`
+	City  string `json:"city"`
+}
+
+type Response struct {
+	StatusCode   int    `json:"status_code"`
+	ErrorMessage string `json:"error_message"`
+	Data         any    `json:"data"`
+}
