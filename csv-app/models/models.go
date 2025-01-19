d137c476a6027bd1cb794e34ac6190ed2399cefe
anudevSaraswat <anudev@tech-x.com>
2025-01-19 14:08:27 +0530

    in progress: user_id field added in User model

--- csv-app/models/models.go
+++ csv-app/models/models.go
@@ -2,10 +2,11 @@ package models
 
 // this struct can be used to scan user data in and for sending filters for querying data
 type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	DOB   string `json:"dob"`
-	City  string `json:"city"`
+	Name   string `json:"name"`
+	UserID string `json:"user_id"`
+	Email  string `json:"email"`
+	DOB    string `json:"dob"`
+	City   string `json:"city"`
 }
 
 type Response struct {
