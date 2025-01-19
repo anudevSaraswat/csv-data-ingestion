1c9e8cec78a1f88c3645242433794e9cac0b631a
anudevSaraswat <anudev@tech-x.com>
2025-01-19 14:09:43 +0530

    in progress: user_id column added in User table

--- csv-app/schema/User.sql
+++ csv-app/schema/User.sql
@@ -12,13 +12,15 @@ CREATE DATABASE "golang-test"
 CREATE TABLE IF NOT EXISTS public.user_data
 (
     id integer NOT NULL,
+    user_id character varying(80) COLLATE pg_catalog."default" NOT NULL,
     name character varying(80) COLLATE pg_catalog."default" NOT NULL,
     email character varying(80) COLLATE pg_catalog."default" NOT NULL,
     dob date NOT NULL,
     city character varying(80) COLLATE pg_catalog."default",
     CONSTRAINT user_data_pkey PRIMARY KEY (id),
     CONSTRAINT user_data_email_key UNIQUE (email),
-    CONSTRAINT user_data_name_key UNIQUE (name)
+    CONSTRAINT user_data_name_key UNIQUE (name),
+    CONSTRAINT user_data_user_id_key UNIQUE (user_id)
 )
 
 ALTER TABLE IF EXISTS public."user-data"
