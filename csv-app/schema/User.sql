7692e1ff8e456214d158b6a8176b505a019a57b9
anudevSaraswat <anudev@tech-x.com>
2025-01-19 11:33:09 +0530

    in progress: added db schema file

---
+++ csv-app/schema/User.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE "golang-test"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE TABLE IF NOT EXISTS public.user_data
+(
+    id integer NOT NULL,
+    name character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    dob date NOT NULL,
+    city character varying(80) COLLATE pg_catalog."default",
+    CONSTRAINT user_data_pkey PRIMARY KEY (id),
+    CONSTRAINT user_data_email_key UNIQUE (email),
+    CONSTRAINT user_data_name_key UNIQUE (name)
+)
+
+ALTER TABLE IF EXISTS public."user-data"
+    OWNER to postgres;
