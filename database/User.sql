b3412e7d858db0050a64d3b02616dfcfd82e02e0
anudevSaraswat <anudev@tech-x.com>
2025-01-20 02:41:12 +0530

    in progress: - Dockerfile added for postgres, csv-app and csv-producer service

--- database/User.sql
+++ database/User.sql
@@ -1,13 +1,4 @@
-CREATE DATABASE "golang-test"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+CREATE DATABASE "golang-test";
 
 CREATE TABLE IF NOT EXISTS public.user_data
 (
@@ -21,7 +12,7 @@ CREATE TABLE IF NOT EXISTS public.user_data
     CONSTRAINT user_data_email_key UNIQUE (email),
     CONSTRAINT user_data_name_key UNIQUE (name),
     CONSTRAINT user_data_user_id_key UNIQUE (user_id)
-)
+);
 
 ALTER TABLE IF EXISTS public."user-data"
     OWNER to postgres;
